9e16a9dc96d3c198967e89348c450602c53e589b
Taofeeq001 <taofeeqkolapo@gmail.com>
2023-08-07 09:09:47 -0400

    updating hero drop down

--- src/Components/Serp.jsx
+++ src/Components/Serp.jsx
@@ -11,19 +11,22 @@ const Serp = () =>{
         if (!search){
             setError("Input the name of your client")
         }
+        const myHeader = new Headers();
+        myHeader.append("X-API-Key",key);
+        myHeader.append("content-type","application/json")
         const body = {
-            "place": search
+            "q": search
         };
-        const base_url = `https://google.serper.dev/places`;
-        const head = {
-            key,
-            "content-type":"application/json"
-        } 
+        const base_url = `https:s//google.serper.dev/places`;
+        // const head = {
+            
+            
+        // } 
         try {
             const fetchData = async () =>{
                 await fetch(base_url, {
                     method:'POST',
-                    headers: head,
+                    headers: myHeader,
                     body: JSON.stringify (body),
                     redirect:'follow',
                     mode: "no-cors"
@@ -31,6 +34,7 @@ const Serp = () =>{
                 .then(res=>res)
                 .then(response => console.log(response))
             }
+        // console.log(search);
             fetchData()
         } catch (error) {
             
